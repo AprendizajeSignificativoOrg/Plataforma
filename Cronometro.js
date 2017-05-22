c551991d32dca13f805589635a71bc9484bc45e7
BibianS <practicantesoftware@aprendizajesignificativo.org>
2017-05-22 12:44:30 -0500

    Add files via upload

---
+++ Cronometro.js
@@ -0,0 +1,33 @@
+//Cronometro 
+
+function tiempo(min){//Función principal del cronometro
+var segundos=0;//inicialización de la variable segundos
+var minutos=min; //inicialización de la variable minutos según parametro (html)
+var tiempos=function(){//función de control cronometro
+
+
+	segundos=segundos-1;// asignación valor a variable
+	if(segundos<0)//ciclo para cronometro
+	{
+		segundos=59;//asignación del valor 
+		minutos=minutos-1;//asignación de valor a varible
+	}
+	form.second.value=segundos;// imprime segundos
+
+	form.minute.value=minutos;//imprime minutos
+	if(minutos<0)//ciclo para finalizar cronometro
+	{
+		form.second.value=0;//impreime variable en 0
+		form.minute.value=0;//imprime variable en 0
+	}else{
+		setTimeout(tiempos,1000);// tiempo de recorrido del ciclo cronometro
+	}
+	
+	if (segundos==0 && minutos==0) {//cmparar tiempo para finalizar y accionar el botón
+		btn.onclick=compararRespuestas(this);//accciona el boton enviar
+		btn.click();
+	}
+
+}
+return tiempos();
+}
