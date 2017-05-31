0aac472275216677f9aa31b8004ab7201ff332b4
BibianS <practicantesoftware@aprendizajesignificativo.org>
2017-05-31 13:55:45 -0500

    Add files via upload

---
+++ RespuestasBADYGMMemoriaOrtografica.js
@@ -0,0 +1,271 @@
+//Matriz de respuestas correctas y respondidas
+function matrizRespuestasMemoria(matrizRespuestas){
+
+  matrizRespuestas[1][0]="1";//item
+  matrizRespuestas[1][1]="m";//respuesta correcta
+  matrizRespuestas[1][2]="";//respuesta estudiante
+
+  matrizRespuestas[2][0]="2";
+  matrizRespuestas[2][1]="r";
+  matrizRespuestas[2][2]="";
+
+  matrizRespuestas[3][0]="3";
+  matrizRespuestas[3][1]="w";
+    matrizRespuestas[3][2]="";
+
+  matrizRespuestas[4][0]="4";
+  matrizRespuestas[4][1]="c";
+  matrizRespuestas[4][2]="";
+
+  matrizRespuestas[5][0]="5";
+  matrizRespuestas[5][1]="g";
+  matrizRespuestas[5][2]="";
+
+  matrizRespuestas[6][0]="6";   
+  matrizRespuestas[6][1]="k";
+  matrizRespuestas[6][2]="";
+
+  matrizRespuestas[7][0]="7";
+  matrizRespuestas[7][1]="p";
+  matrizRespuestas[7][2]="";
+
+    matrizRespuestas[8][0]="8";
+  matrizRespuestas[8][1]="w";
+  matrizRespuestas[8][2]="";
+
+  matrizRespuestas[9][0]="9";
+  matrizRespuestas[9][1]="b";
+  matrizRespuestas[9][2]="";
+
+  matrizRespuestas[10][0]="10";
+  matrizRespuestas[10][1]="f";
+  matrizRespuestas[10][2]="";
+  
+  matrizRespuestas[11][0]="11";
+  matrizRespuestas[11][1]="l";
+  matrizRespuestas[11][2]="";
+  
+  matrizRespuestas[12][0]="12";
+  matrizRespuestas[12][1]="r";
+  matrizRespuestas[12][2]="";
+
+  matrizRespuestas[13][0]="13";
+  matrizRespuestas[13][1]="v";
+  matrizRespuestas[13][2]="";
+
+  matrizRespuestas[14][0]="14";
+  matrizRespuestas[14][1]="c";
+  matrizRespuestas[14][2]="";
+
+  matrizRespuestas[15][0]="15";
+  matrizRespuestas[15][1]="g";
+  matrizRespuestas[15][2]="";
+
+  matrizRespuestas[16][0]="16";
+  matrizRespuestas[16][1]="k";
+  matrizRespuestas[16][2]="";
+
+  matrizRespuestas[17][0]="17";
+  matrizRespuestas[17][1]="q";
+  matrizRespuestas[17][2]="";
+
+  matrizRespuestas[18][0]="18";
+  matrizRespuestas[18][1]="x";
+  matrizRespuestas[18][2]="";
+
+  matrizRespuestas[19][0]="19";
+  matrizRespuestas[19][1]="c";
+  matrizRespuestas[19][2]="";
+
+  matrizRespuestas[20][0]="20";
+  matrizRespuestas[20][1]="f";
+  matrizRespuestas[20][2]="";
+
+  matrizRespuestas[21][0]="21";
+  matrizRespuestas[21][1]="k";
+  matrizRespuestas[21][2]="";
+
+  matrizRespuestas[22][0]="22";
+  matrizRespuestas[22][1]="q";
+  matrizRespuestas[22][2]="";
+
+  matrizRespuestas[23][0]="23";
+  matrizRespuestas[23][1]="v";
+  matrizRespuestas[23][2]="";
+
+  matrizRespuestas[24][0]="24";
+  matrizRespuestas[24][1]="c";
+  matrizRespuestas[24][2]="";
+
+  matrizRespuestas[25][0]="25";
+  matrizRespuestas[25][1]="g";
+  matrizRespuestas[25][2]="";
+  
+  matrizRespuestas[26][0]="26";
+  matrizRespuestas[26][1]="k";
+  matrizRespuestas[26][2]="";
+
+  matrizRespuestas[27][0]="27";
+  matrizRespuestas[27][1]="r";
+  matrizRespuestas[27][2]="";
+
+  matrizRespuestas[28][0]="28";
+  matrizRespuestas[28][1]="w";
+  matrizRespuestas[28][2]="";
+
+  matrizRespuestas[29][0]="29";
+  matrizRespuestas[29][1]="c";
+  matrizRespuestas[29][2]="";
+
+  matrizRespuestas[30][0]="30";
+  matrizRespuestas[30][1]="f";
+  matrizRespuestas[30][2]="";
+
+  matrizRespuestas[31][0]="31";
+  matrizRespuestas[31][1]="k";
+  matrizRespuestas[31][2]="";
+
+  matrizRespuestas[32][0]="32";
+  matrizRespuestas[32][1]="q";
+  matrizRespuestas[32][2]="";
+  
+  
+ }
+
+//Matriz Baremacion 
+ function matrizBaremacionMemoria(matrizBaremacion){
+
+  matrizBaremacion[1][0]="0";//respuestas correctas obtenidas
+  matrizBaremacion[1][1]="1";//puntaje convertido
+  matrizBaremacion[1][2]="Muy Bajo";//escala
+
+  matrizBaremacion[2][0]="1";
+  matrizBaremacion[2][1]="1";
+  matrizBaremacion[2][2]="Muy Bajo";
+
+  matrizBaremacion[3][0]="2";
+  matrizBaremacion[3][1]="1";
+    matrizBaremacion[3][2]="Muy Bajo";
+
+  matrizBaremacion[4][0]="3";
+  matrizBaremacion[4][1]="1";
+  matrizBaremacion[4][2]="Muy Bajo";
+
+  matrizBaremacion[5][0]="4";
+  matrizBaremacion[5][1]="1";
+  matrizBaremacion[5][2]="Muy Bajo";
+
+  matrizBaremacion[6][0]="5";   
+  matrizBaremacion[6][1]="1";
+  matrizBaremacion[6][2]="Muy Bajo";
+
+  matrizBaremacion[7][0]="6";
+  matrizBaremacion[7][1]="2";
+  matrizBaremacion[7][2]="Muy Bajo";
+
+    matrizBaremacion[8][0]="7";
+  matrizBaremacion[8][1]="2";
+  matrizBaremacion[8][2]="Muy Bajo";
+
+  matrizBaremacion[9][0]="8";
+  matrizBaremacion[9][1]="3";
+  matrizBaremacion[9][2]="Muy Bajo";
+
+  matrizBaremacion[10][0]="9";
+  matrizBaremacion[10][1]="4";
+  matrizBaremacion[10][2]="Muy Bajo";
+  
+  matrizBaremacion[11][0]="10";
+  matrizBaremacion[11][1]="6";
+  matrizBaremacion[11][2]="Muy Bajo";
+  
+  matrizBaremacion[12][0]="11";
+  matrizBaremacion[12][1]="9";
+  matrizBaremacion[12][2]="Bajo";
+
+  matrizBaremacion[13][0]="12";
+  matrizBaremacion[13][1]="13";
+  matrizBaremacion[13][2]="Bajo";
+
+  matrizBaremacion[14][0]="13";
+  matrizBaremacion[14][1]="15";
+  matrizBaremacion[14][2]="Medio Bajo";
+
+  matrizBaremacion[15][0]="14";
+  matrizBaremacion[15][1]="20";
+  matrizBaremacion[15][2]="Medio bajo";
+
+  matrizBaremacion[16][0]="15";
+  matrizBaremacion[16][1]="23";
+  matrizBaremacion[16][2]="Medio bajo";
+
+  matrizBaremacion[17][0]="16";
+  matrizBaremacion[17][1]="30";
+  matrizBaremacion[17][2]="Medio";
+
+  matrizBaremacion[18][0]="17";
+  matrizBaremacion[18][1]="38";
+  matrizBaremacion[18][2]="Medio";
+
+  matrizBaremacion[19][0]="18";
+  matrizBaremacion[19][1]="41";
+  matrizBaremacion[19][2]="Medio";
+
+  matrizBaremacion[20][0]="19";
+  matrizBaremacion[20][1]="50";
+  matrizBaremacion[20][2]="Medio";
+
+  matrizBaremacion[21][0]="20";
+  matrizBaremacion[21][1]="54";
+  matrizBaremacion[21][2]="Medio";
+
+  matrizBaremacion[22][0]="21";
+  matrizBaremacion[22][1]="59";
+  matrizBaremacion[22][2]="Medio";
+
+  matrizBaremacion[23][0]="22";
+  matrizBaremacion[23][1]="62";
+  matrizBaremacion[23][2]="Medio";
+
+  matrizBaremacion[24][0]="23";
+  matrizBaremacion[24][1]="70";
+  matrizBaremacion[24][2]="Medio Alto";
+
+  matrizBaremacion[25][0]="24";
+  matrizBaremacion[25][1]="73";
+  matrizBaremacion[25][2]="Medio Alto";
+  
+  matrizBaremacion[26][0]="25";
+  matrizBaremacion[26][1]="80";
+  matrizBaremacion[26][2]="Medio Alto";
+
+  matrizBaremacion[27][0]="26";
+  matrizBaremacion[27][1]="82";
+  matrizBaremacion[27][2]="Medio Alto";
+
+  matrizBaremacion[28][0]="27";
+  matrizBaremacion[28][1]="87";
+  matrizBaremacion[28][2]="Alto";
+
+  matrizBaremacion[29][0]="28";
+  matrizBaremacion[29][1]="89";
+  matrizBaremacion[29][2]="Alto";
+
+  matrizBaremacion[30][0]="29";
+  matrizBaremacion[30][1]="94";
+  matrizBaremacion[30][2]="Muy Alto";
+
+  matrizBaremacion[31][0]="30";
+  matrizBaremacion[31][1]="96";
+  matrizBaremacion[31][2]="Muy Alto";
+
+  matrizBaremacion[32][0]="31";
+  matrizBaremacion[32][1]="98";
+  matrizBaremacion[32][2]="Muy Alto";
+
+  matrizBaremacion[33][0]="32";
+  matrizBaremacion[33][1]="100";
+  matrizBaremacion[33][2]="Muy Alto";
+  
+ }
+
