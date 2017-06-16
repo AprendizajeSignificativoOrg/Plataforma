9440d69c79b62463d9e6169eb28492b1a0c3a8c9
DesarrolladorAS <desarrollador@aprendizajesignificativo.org>
2017-06-16 13:02:40 -0500

    Add files via upload

--- Respuestas.js
+++ Respuestas.js
@@ -11,8 +11,10 @@ function recogeRespuestasContenedores (pregunta, respuesta, respuestas, contened
     cadenaPregunta = pregunta.split('a'); //Divide en dos la cadena "cadenaPregunta" antes de 'a' y después de 'a'
     var numeroPregunta = Number(cadenaPregunta[1]);//Convierte cadena a número (Obtiene número ítem).
 
-    respuestas[numeroPregunta] = cambiaContenedorxValor(respuesta); //Llamado función para obtener el valor del contenedor (Respuesta).
+    var numeroRespuesta = cambiaContenedorxValor(respuesta);
 
+    respuestas[numeroPregunta] = numeroRespuesta; //Llamado función para obtener el valor del contenedor (Respuesta).
+    contenedorLleno[numeroRespuesta] = "S";
 }
 
 //Función para obtener el valor del contenedor (Respuesta)
@@ -53,5 +55,80 @@ function imprimeRespuestas(respuestas){
       document.write(i + " . ");
       document.write(respuestas[i] + "<br/>");
     }
+    var resultadoA = 0;
+    var resultadoB = 0;
+    var resultadoC = 0;
+    var resultadoD = 0;
+    var resultadoE = 0;
+    var resultadoF = 0;
+
+    resultadoA = respuestas[1] + respuestas[7] + respuestas[17] + respuestas[21] + respuestas[30] + respuestas[36] + respuestas[37] + respuestas[43] + respuestas[50] + respuestas[55];  
+    resultadoB = respuestas[4] + respuestas[10] + respuestas[18] + respuestas[24] + respuestas[29] + respuestas[35] + respuestas[40] + respuestas[48] + respuestas[53] + respuestas[57];
+    resultadoC = respuestas[2] + respuestas[9] + respuestas[13] + respuestas[19] + respuestas[27] + respuestas[31] + respuestas[38] + respuestas[45] + respuestas[51] + respuestas[60];  
+    resultadoD = respuestas[5] + respuestas[12] + respuestas[15] + respuestas[22] + respuestas[28] + respuestas[32] + respuestas[41] + respuestas[47] + respuestas[54] + respuestas[58];
+    resultadoE = respuestas[3] + respuestas[11] + respuestas[14] + respuestas[20] + respuestas[25] + respuestas[34] + respuestas[39] + respuestas[46] + respuestas[52] + respuestas[56];
+    resultadoF = respuestas[6] + respuestas[8] + respuestas[16] + respuestas[23] + respuestas[26] + respuestas[33] + respuestas[42] + respuestas[44] + respuestas[49] + respuestas[59];
+  
+    document.write("<br/> Resultado A = " + resultadoA + "<br/>");
+    document.write("Resultado B = " + resultadoB + "<br/>");
+    document.write("Resultado C = " + resultadoC + "<br/>");
+    document.write("Resultado D = " + resultadoD + "<br/>");
+    document.write("Resultado E = " + resultadoE + "<br/>");
+    document.write("Resultado F = " + resultadoF + "<br/>");
 } 
      
+
+function identificaContenedorVacio(respuesta, contenedorLleno){
+  var numeroRespuesta = cambiaContenedorxValor(respuesta);
+
+  if (contenedorLleno[numeroRespuesta] == "S"){
+    alert ("contenedor Lleno");
+    return contenedorVacio = false;
+  }
+  else {
+    return contenedorVacio = true;
+  }
+}
+
+function vaciaContenedor (nombrePregunta, contenedorLleno){
+    switch (nombrePregunta){
+
+      case 'Contenedor1':
+        contenedorLleno[1] = "N";
+        break;  
+
+      case 'Contenedor2':
+        contenedorLleno[2] = "N";
+        break;  
+
+      case 'Contenedor3':
+        contenedorLleno[3] = "N";
+        break;  
+
+      case 'Contenedor4':
+        contenedorLleno[4] = "N";
+        break;  
+
+      case 'Contenedor5':
+        contenedorLleno[5] = "N";
+        break;  
+
+      case 'Contenedor6':
+        contenedorLleno[6] = "N";
+        break;
+  }
+
+}
+
+function vaciaResultado (itemDesplazado, respuestas){
+  var cadenaPregunta = new Array (); //Crea arreglo para guardar split.
+  cadenaPregunta = itemDesplazado.split('a'); //Divide en dos la cadena "cadenaPregunta" antes de 'a' y después de 'a'
+  var numeroPregunta = Number(cadenaPregunta[1]);//Convierte cadena a número (Obtiene número ítem).
+  respuestas[numeroPregunta] = 0;
+}
+
+function reiniciarContenedor(contenedorLleno){
+  for (i=1; i<7; i++){
+    contenedorLleno[i] = "N";
+  }
+}
---
+++ TablaBaremosIPUParte1.js
@@ -0,0 +1,77 @@
+function creacionTablaBaremosIPUParte1(matrizBaremos){
+	
+	matrizBaremosParte1[0] = "Clave";
+
+	matrizBaremos[1] = "A";
+	matrizBaremos[2] = "C";
+	matrizBaremos[3] = "E";
+	matrizBaremos[4] = "B";
+	matrizBaremos[5] = "D";
+	matrizBaremos[6] = "F";	
+
+	matrizBaremos[7] = "A";
+	matrizBaremos[8] = "F";
+	matrizBaremos[9] = "C";
+	matrizBaremos[10] = "B";
+	matrizBaremos[11] = "E";
+	matrizBaremos[12] = "D";
+
+	matrizBaremos[13] = "C";
+	matrizBaremos[14] = "E";
+	matrizBaremos[15] = "D";
+	matrizBaremos[16] = "F";
+	matrizBaremos[17] = "A";
+	matrizBaremos[18] = "B";
+
+	matrizBaremos[19] = "C";
+	matrizBaremos[20] = "E";
+	matrizBaremos[21] = "A";
+	matrizBaremos[22] = "D";
+	matrizBaremos[23] = "F";
+	matrizBaremos[24] = "B";
+
+	matrizBaremos[25] = "E";
+	matrizBaremos[26] = "F";
+	matrizBaremos[27] = "C";
+	matrizBaremos[28] = "D";
+	matrizBaremos[29] = "B";
+	matrizBaremos[30] = "A";
+
+	matrizBaremos[31] = "C";
+	matrizBaremos[32] = "D";
+	matrizBaremos[33] = "F";
+	matrizBaremos[34] = "E";
+	matrizBaremos[35] = "B";
+	matrizBaremos[36] = "A";
+
+	matrizBaremos[37] = "A";
+	matrizBaremos[38] = "C";
+	matrizBaremos[39] = "E";
+	matrizBaremos[40] = "B";
+	matrizBaremos[41] = "D";
+	matrizBaremos[42] = "F";
+
+	matrizBaremos[43] = "A";
+	matrizBaremos[44] = "F";
+	matrizBaremos[45] = "C";
+	matrizBaremos[46] = "E";
+	matrizBaremos[47] = "D";
+	matrizBaremos[48] = "B";
+
+	matrizBaremos[49] = "F";
+	matrizBaremos[50] = "A";
+	matrizBaremos[51] = "C";
+	matrizBaremos[52] = "E";
+	matrizBaremos[53] = "B";
+	matrizBaremos[54] = "D";
+
+	matrizBaremos[55] = "A";
+	matrizBaremos[56] = "E";
+	matrizBaremos[57] = "B";
+	matrizBaremos[58] = "D";
+	matrizBaremos[59] = "F";
+	matrizBaremos[60] = "C";
+} 
+
+
+
