d22ceddc80fc97ed0fcfc12ab6df86332c187560
BibianS <practicantesoftware@aprendizajesignificativo.org>
2017-06-23 14:34:27 -0500

    Add files via upload

---
+++ ResultadosBADYGSEncajar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resultados BADYG S</title>
+	<meta charset="utf-8">
+</head>
+<body>
+<?php
+echo "<b>Encajar Figuras</b><br>";
+$CorrectasEF=$_COOKIE["CorrEF"];
+$BaremacionEF=$_COOKIE["BareEF"];
+$EscalaEF=$_COOKIE["EsclEF"];
+echo "Puntaje Directo Obtenido por el estudiante ".$CorrectasEF." Baremación ".$BaremacionEF."% Escala ".$EscalaEF;
+?>
+</body>
+</html>
